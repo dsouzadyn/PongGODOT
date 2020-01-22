85ad7c8a73a3bf527c8c6a21c8455d70e25c4c36
Dylan Dsouza <dsouzadyn@gmail.com>
2020-01-22 23:15:01 +0530

    add sounds and crt shader

--- actors/Ball.gd
+++ actors/Ball.gd
@@ -9,12 +9,14 @@ var game_over_label
 var direction = Vector2(-1.0, 1.0)
 export (int) var ball_speed = 200
 var is_game_over
+var sound_player
 func _ready():
 	screen_size = get_viewport_rect().size
 	player_one_score_label = $"../PlayerOneScore"
 	player_two_score_label = $"../PlayerTwoScore"
 	game_over_label = $"../GameOverLabel"
 	is_game_over = get_node("../../Arena").game_over
+	sound_player = get_node("../AudioStreamPlayer2D")
 
 func _physics_process(delta):
 	is_game_over = get_node("../../Arena").game_over
@@ -24,8 +26,10 @@ func _physics_process(delta):
 			var collider = collision.get_collider()
 			if collider.name == 'Paddle' or collider.name == 'PaddleTwo':
 				direction.x = -direction.x
+				sound_player.play()
 			if collider.name == 'TopWall' or collider.name == 'BottomWall':
 				direction.y = -direction.y
+				sound_player.play()
 			if collider.name == 'Paddle':
 				player_one_score += 1
 				player_one_score_label.text = str(player_one_score)
